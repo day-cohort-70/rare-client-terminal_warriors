1a19698fa65eac8fff2ef5fb410e6b0d604145dc
Oliver Kennelly <victoriakennelly@gmail.com>
2024-05-31 10:26:37 -0500

    mostly carved out

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { AllPosts } from "../components/posts/AllPosts"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -9,7 +10,7 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
-        {/* Add Routes here */}
+        {<Route path="/allposts" element={<AllPosts/>}/>}
         
       </Route>
     </Routes>
