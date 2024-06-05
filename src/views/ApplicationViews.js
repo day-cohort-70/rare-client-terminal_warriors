22fca2cf8a13d6d770da587584c8b6962ff4a6ec
William R. Stetar <kebekad673@proton.me>
2024-06-05 13:53:46 -0500

    Add route for CreatePosts component and update ApplicationViews.js

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,30 +1,28 @@
-import { Route, Routes } from "react-router-dom"
-import { Login } from "../components/auth/Login"
-import { Register } from "../components/auth/Register"
-import { Authorized } from "./Authorized"
-import { AllPosts } from "../components/posts/AllPosts"
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/auth/Login";
+import { Register } from "../components/auth/Register";
+import { Authorized } from "./Authorized";
+import { AllPosts } from "../components/posts/AllPosts";
 import { TagForm } from '../components/tags/TagForm';
 import { TagList } from '../components/tags/TagList.js';
 import { CategoryList } from '../components/categories/CategoryList.js';
-import { EditCategory } from "../components/categories/EditCategory.js"
-
+import { EditCategory } from "../components/categories/EditCategory.js";
+import { CreatePosts } from '../components/posts/CreatePosts'; // Import CreatePosts component
 
 export const ApplicationViews = ({ token, setToken }) => {
-
   return (
     <Routes>
       <Route path="/login" element={<Login setToken={setToken} />} />
       <Route path="/register" element={<Register setToken={setToken} />} />
       <Route element={<Authorized token={token} />}>
-
         <Route path="/tags/create" element={<TagForm />} />
         <Route path='/tags' element={<TagList />} />
         <Route path='/categories'>
-          <Route index element={<CategoryList/>}/>
-          <Route path=":categoryId" element={<EditCategory/>}/>
+          <Route index element={<CategoryList />} />
+          <Route path=":categoryId" element={<EditCategory />} />
         </Route>
-        <Route path="/allposts" element={<AllPosts/>}/>
-
+        <Route path="/allposts" element={<AllPosts />} />
+        <Route path="/posts/create" element={<CreatePosts />} /> {/* New route for CreatePosts */}
       </Route>
     </Routes>
   );
