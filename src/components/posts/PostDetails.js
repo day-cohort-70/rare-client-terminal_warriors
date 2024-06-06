54b6351b733984dea5bb5a91a8b830fc19c5f933
Oliver Kennelly <victoriakennelly@gmail.com>
2024-06-06 11:18:14 -0500

    all items listed on post details

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -12,5 +12,31 @@ export const PostDetails = () => {
         })
     }, [postId])
 
-    return <div>{postInfo.content}</div>
+    return <div>
+        <h2>{postInfo.title}</h2>
+        <div>{postInfo.category_name}</div>
+        <img src={postInfo.image_url}/>
+        <div>{postInfo.tags}</div>
+        <div>By {postInfo.author}</div>
+        <div>{postInfo.content}</div>
+    </div>
 }
+
+/* 
+for future reference, postInfo looks something like this:
+
+{
+    "id": 3,
+    "user_id": 4,
+    "title": "Cooking Recipe",
+    "publication_date": "2023-05-15",
+    "author": "Michael Johnson",
+    "category_name": "Travel",
+    "image_url": "https://example.com/image3.jpg",
+    "content": "Here is a recipe for a delicious pasta dish.",
+    "tags": [
+        "Travel",
+        "Fashion"
+    ]
+}
+*/
